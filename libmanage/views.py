9b8496e2659711e5316318846f1510fd8251b056
Ksholla123m <hollask20@gmail.com>
2022-06-25 13:26:36 +0530

    upload librarymanagement

--- libmanage/views.py
+++ libmanage/views.py
@@ -15,7 +15,7 @@ def library(request):
 
 def collection(request):
     all_books = Book.objects.all()
-    paginator = Paginator(all_books, 4)
+    paginator = Paginator(all_books, 6)
     page = request.GET.get('pg')
     all_books = paginator.get_page(page)
    
--- librarymanagement/settings.py
+++ librarymanagement/settings.py
@@ -29,6 +29,7 @@ INSTALLED_APPS = [
     'libmanage',
     'users_app',
     'crispy_forms',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
@@ -67,8 +68,12 @@ WSGI_APPLICATION = 'librarymanagement.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql', 
+        'NAME': 'library_management',
+        'USER': 'root',
+        'PASSWORD': 'harekrishna',
+        'HOST': 'localhost',   # Or an IP Address that your DB is hosted on
+        'PORT': '3306',
     }
 }
 
