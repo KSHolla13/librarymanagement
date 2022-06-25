1612ec3989771c74c4ee57f3090001478223015d
Ksholla123m <hollask20@gmail.com>
2022-06-25 15:01:56 +0530

    settings updated

--- librarymanagement/settings.py
+++ librarymanagement/settings.py
@@ -71,7 +71,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql', 
         'NAME': 'library_management',
         'USER': 'root',
-        'PASSWORD': 'harekrishna',
+        'PASSWORD': '',
         'HOST': 'localhost',  
         'PORT': '3306',
     }
