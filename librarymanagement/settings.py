5d4d8f194f4022fc4627617aaa6b299390a9fd82
Ksholla123m <hollask20@gmail.com>
2022-06-26 21:25:21 +0530

    heroku & env added

--- librarymanagement/settings.py
+++ librarymanagement/settings.py
@@ -1,20 +1,19 @@
 import os
 from pathlib import Path
+import environ
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
+env = environ.Env(SECRET_KEY = str,)
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-=zia^e2!!#htj*^kne!q$9&trrige62n!z$an8b(!r8*l##xp6'
+SECRET_KEY = env('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env('DJANGO_DEBUG')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [env('DJANGO_ALLOWED_HOST')]
 
 
 # Application definition
@@ -71,7 +70,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql', 
         'NAME': 'library_management',
         'USER': 'root',
-        'PASSWORD': '',
+        'PASSWORD': 'harekrishna',
         'HOST': 'localhost',  
         'PORT': '3306',
     }
